e137a3e5b7289cd1b442eddb2f5fae32cd449864
Julio Cesar <jcestudo1.0@gmail.com>
2026-06-22 20:30:04 -0300

    Initial commit

---
+++ src/aprendendo_do_zero/exercicio01.java
@@ -0,0 +1,9 @@
+package aprendendo_do_zero;
+
+public class exercicio01 {
+    public static void main(String[] args) {
+        String nome = "Julio";
+        int idade = 21;
+        System.out.println("Olá mundo! Meu nome é " + nome + " tenho " + idade + " anos, sou do Brasil, SP, e estou aprendendo backénd.");
+    }
+}
