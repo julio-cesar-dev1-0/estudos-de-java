c4b3fd675dacd7d7479aa5db4bb12c60adf47063
Julio Cesar <jcestudo1.0@gmail.com>
2026-06-22 20:35:59 -0300

    corrigindo escrita de back-end.

--- src/aprendendo_do_zero/exercicio01.java
+++ src/aprendendo_do_zero/exercicio01.java
@@ -4,6 +4,6 @@ public class exercicio01 {
     public static void main(String[] args) {
         String nome = "Julio";
         int idade = 21;
-        System.out.println("Olá mundo! Meu nome é " + nome + " tenho " + idade + " anos, sou do Brasil, SP, e estou aprendendo backénd.");
+        System.out.println("Olá mundo! Meu nome é " + nome + " tenho " + idade + " anos, sou do Brasil, SP, e estou aprendendo back-end.");
     }
 }
